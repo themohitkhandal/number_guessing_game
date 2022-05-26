5a28520ff846fa755b1925a33a89702dbf3649fc
Mohit Windows <mohit1319khandal@gmail.com>
2022-05-26 15:09:33 +0530

    number guessing game

---
+++ script.js
@@ -0,0 +1,54 @@
+const button = document.getElementById('btn')
+const feed = document.getElementById('feedback')
+const showChance = document.getElementById('chance')
+
+function userName() {
+    let user = prompt("Enter your name");
+
+    if (user != null) {
+        document.getElementById("username").innerHTML = "Hello " + user;
+    }
+}
+
+function resetPage() {
+    document.location.reload(true);
+}
+
+
+
+let randNumber = Math.floor(Math.random() * 100) + 1;
+let chance = 10;
+console.log(randNumber)
+
+
+
+function check() {
+    let userInput = document.getElementById('userInput').value;
+    if (userInput != null) {
+        if (userInput == randNumber) {
+            // console.log("You gueesd it right" + randNumber)
+            feed.innerHTML = "Congratulations! You guessed it correct"
+        } else if (userInput > randNumber) {
+            // console.log("number too high")
+            feed.innerHTML = "Number too high"
+        } else if (userInput < randNumber) {
+            // console.log("number too low")
+            feed.innerHTML = "Number too low"
+        } else {
+            console.log("something wennt wrong")
+        }
+        chance--;
+    }
+    if(chance == 0){
+        // console.log("You ran out of chances")
+        feed.innerHTML = "You ran out of chances"
+    }
+    console.log(chance)
+    showChance.innerHTML = chance
+    
+}
+console.log(chance)
+showChance.innerHTML = chance
+
+
+
